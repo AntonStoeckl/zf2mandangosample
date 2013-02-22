269354eba56f27214bed82f8b1149c3029c66e69
Anton Stöckl <anton@stoeckl.de>
2013-02-22 10:35:29 +0100

    added doc blocks

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -3,9 +3,13 @@
 namespace Album\Model;
 
 use AntonStoeckl\Resources\AlbumRepositoryInterface;
+use AntonStoeckl\Resources\AlbumItemInterface;
 
 class Album
 {
+    /**
+     * @var \AntonStoeckl\Resources\AlbumRepositoryInterface
+     */
     protected $albumRepository;
 
     public function __construct(AlbumRepositoryInterface $albumRepository)
@@ -13,21 +17,42 @@ class Album
         $this->albumRepository = $albumRepository;
     }
 
+    /**
+     * @return \AntonStoeckl\Resources\AlbumRepositoryInterface
+     */
+    public function getAlbumRepository()
+    {
+        return $this->albumRepository;
+    }
+
+    /**
+     * @return mixed
+     */
     public function fetchAllAlbums()
     {
         return $this->albumRepository->fetchAllAlbums();
     }
 
+    /**
+     * @param string $id
+     * @return \AntonStoeckl\Resources\AlbumItemInterface
+     */
     public function fetchOneAlbumById($id)
     {
         return $this->albumRepository->fetchOneAlbumById($id);
     }
 
-    public function saveAlbum($album)
+    /**
+     * @param \AntonStoeckl\Resources\AlbumItemInterface $album
+     */
+    public function saveAlbum(AlbumItemInterface $album)
     {
         $this->albumRepository->saveAlbum($album);
     }
 
+    /**
+     * @param string $id
+     */
     public function deleteAlbum($id)
     {
         $this->albumRepository->deleteAlbum($id);
