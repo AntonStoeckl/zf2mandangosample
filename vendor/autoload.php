9d88994e3567c27e93b6113360dad1562a087880
Anton Stöckl <anton@stoeckl.de>
2013-02-25 21:51:36 +0100

    composer update run

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb3fb20fc9657f46111798540dfdb01a4::getLoader();
+return ComposerAutoloaderInited114ad69a3de2e343540698a8676fc2::getLoader();
