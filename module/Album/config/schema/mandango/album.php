022a08a0d6782caa9c752854fc1205a12bfa873f
Anton Stöckl <anton@stoeckl.de>
2013-02-28 14:22:02 +0100

    moved and changed to use full array notation

---
+++ module/Album/config/schema/mandango/album.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'Album\Resource\Mandango\Album' => array(
+        'collection' => 'album',
+        'fields' => array(
+            'artist' => array('type' => 'string', 'dbName' => 'artist'),
+            'title'  => array('type' => 'string', 'dbName' => 'artist'),
+        ),
+    ),
+);
+
