854d1bab6c6c0b7b1eb9f7e8133c878bc55da562
Anton Stöckl <anton@stoeckl.de>
2013-02-28 18:46:31 +0100

    ignore vendor

--- module/Album/src/Album/Resource/Mandango/Mapping/MetadataFactory.php
+++ module/Album/src/Album/Resource/Mandango/Mapping/MetadataFactory.php
@@ -5,6 +5,8 @@ namespace Album\Resource\Mandango\Mapping;
 class MetadataFactory extends \Mandango\MetadataFactory
 {
     protected $classes = array(
+        'Album\\Resource\\Mandango\\Testsubdoc' => true,
         'Album\\Resource\\Mandango\\Album' => false,
+        'Album\\Resource\\Mandango\\Testdoc' => false,
     );
 }
--- module/Album/src/Album/Resource/Mandango/Mapping/MetadataFactoryInfo.php
+++ module/Album/src/Album/Resource/Mandango/Mapping/MetadataFactoryInfo.php
@@ -4,6 +4,44 @@ namespace Album\Resource\Mandango\Mapping;
 
 class MetadataFactoryInfo
 {
+    public function getAlbumResourceMandangoTestsubdocClass()
+    {
+        return array(
+            'isEmbedded' => true,
+            'inheritable' => false,
+            'inheritance' => false,
+            'fields' => array(
+                'foo' => array(
+                    'dbName' => 'foo',
+                    'type' => 'string',
+                ),
+                'bar' => array(
+                    'dbName' => 'bar',
+                    'type' => 'string',
+                ),
+            ),
+            '_has_references' => false,
+            'referencesOne' => array(
+
+            ),
+            'referencesMany' => array(
+
+            ),
+            'embeddedsOne' => array(
+
+            ),
+            'embeddedsMany' => array(
+
+            ),
+            'indexes' => array(
+
+            ),
+            '_indexes' => array(
+
+            ),
+        );
+    }
+
     public function getAlbumResourceMandangoAlbumClass()
     {
         return array(
@@ -32,6 +70,61 @@ class MetadataFactoryInfo
             ),
             'embeddedsOne' => array(
 
+            ),
+            'embeddedsMany' => array(
+
+            ),
+            'relationsOne' => array(
+
+            ),
+            'relationsManyOne' => array(
+
+            ),
+            'relationsManyMany' => array(
+
+            ),
+            'relationsManyThrough' => array(
+
+            ),
+            'indexes' => array(
+
+            ),
+            '_indexes' => array(
+
+            ),
+        );
+    }
+
+    public function getAlbumResourceMandangoTestdocClass()
+    {
+        return array(
+            'isEmbedded' => false,
+            'mandango' => null,
+            'connection' => '',
+            'collection' => 'album',
+            'inheritable' => false,
+            'inheritance' => false,
+            'fields' => array(
+                'artist' => array(
+                    'dbName' => 'artist',
+                    'type' => 'string',
+                ),
+                'title' => array(
+                    'dbName' => 'title',
+                    'type' => 'string',
+                ),
+            ),
+            '_has_references' => false,
+            'referencesOne' => array(
+
+            ),
+            'referencesMany' => array(
+
+            ),
+            'embeddedsOne' => array(
+                'source' => array(
+                    'class' => 'Album\\Resource\\Mandango\\Testsubdoc',
+                ),
             ),
             'embeddedsMany' => array(
 
